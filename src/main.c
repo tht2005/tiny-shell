0b49b1d9107a5da7549f22fd28882d35a58c9665
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-03-07 18:25:23 +0700

    comfirm hash table code style

--- src/main.c
+++ src/main.c
@@ -7,8 +7,10 @@
 #include <string.h>
 
 #include "version.h"
-#include "hash_table.h"
+
 #include "dalloc.h"
+#include "hash_table.h"
+#include "void_stack.h"
 
 struct keyvalue {
     char* key;
@@ -19,11 +21,6 @@ struct keyvalue* new_keyvalue(char* key, char* value) {
     a->key = key;
     a->value = value;
     return a;
-}
-
-void* keyvalue_dup(void* obj) {
-    return (void *)
-        new_keyvalue(((struct keyvalue*)obj)->key, ((struct keyvalue *)obj)->value);
 }
 
 int keyvalue_cmp(void* a, void* b) {
@@ -48,7 +45,29 @@ int main(int argc, char** argv) {
 
     struct hash_table_t* a = new_hash_table(1<<15);
 
-    hash_table_put(a, new_keyvalue("abcd", "an ba to com"), hash, keyvalue_cmp, keyvalue_dup);
+    struct keyvalue* kv;
+    struct linked_list_node_t* ptr;
+
+    kv = new_keyvalue("abcd", "an ba to com");
+    hash_table_put(a, kv, hash, keyvalue_cmp);
+
+    kv = new_keyvalue("abcd", "an ba to com");
+    ptr = hash_table_look_up(a, kv, hash, keyvalue_cmp);
+    free(kv);
+    printf("debug: %s\n", (ptr) ? (char*)(((struct keyvalue *)ptr->ptr)->value) : "(null)");
+
+    kv = new_keyvalue("abcde", "an ba to com");
+    ptr = hash_table_look_up(a, kv, hash, keyvalue_cmp);
+    free(kv);
+    printf("debug: %s\n", (ptr) ? (char*)(((struct keyvalue *)ptr->ptr)->value) : "(null)");
+
+    kv = new_keyvalue("abcd", "an hai to com thoi");
+    hash_table_put(a, kv, hash, keyvalue_cmp);
+
+    kv = new_keyvalue("abcd", "an ba to com");
+    ptr = hash_table_look_up(a, kv, hash, keyvalue_cmp);
+    free(kv);
+    printf("debug: %s\n", (ptr) ? (char*)(((struct keyvalue *)ptr->ptr)->value) : "(null)");
 
     free_hash_table(a);
 
