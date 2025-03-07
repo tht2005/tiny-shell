450db232847fb2f7c26ded14c9f35ba1cdd008b6
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-03-07 15:00:10 +0700

    test hash table

--- src/main.c
+++ src/main.c
@@ -10,24 +10,47 @@
 #include "hash_table.h"
 #include "dalloc.h"
 
-void init() {
-    printf(TINY_SHELL" version %d.%d\n", TINY_SHELL_VERSION_MAJOR, TINY_SHELL_VERSION_MINOR);
+struct keyvalue {
+    char* key;
+    char* value;
+};
+struct keyvalue* new_keyvalue(char* key, char* value) {
+    struct keyvalue* a = (struct keyvalue *) dalloc(sizeof(struct keyvalue));
+    a->key = key;
+    a->value = value;
+    return a;
+}
+
+void* keyvalue_dup(void* obj) {
+    return (void *)
+        new_keyvalue(((struct keyvalue*)obj)->key, ((struct keyvalue *)obj)->value);
+}
+
+int keyvalue_cmp(void* a, void* b) {
+    return strcmp(
+        ((struct keyvalue *)a)->key,
+        ((struct keyvalue *)b)->key
+    );
+}
+
+unsigned hash(void* obj) {
+    char* s = ((struct keyvalue *)obj)->key;
+    unsigned res = 0;
+    for(; *s; ++s) {
+        res = res * 311 + *s + 1;
+    }
+    return res;
 }
 
 char command[5000];
 
 int main(int argc, char** argv) {
 
-    init();
-    
-    while(1) {
-        printf("tiny-shell> $ ");
-        fgets(command, 4000, stdin);
-        printf("Your command: %s\n", command);
-        if(strcmp(command, "exit\n") == 0) {
-            break;
-        }
-    }
+    struct hash_table_t* a = new_hash_table(1<<15);
+
+    hash_table_put(a, new_keyvalue("abcd", "an ba to com"), hash, keyvalue_cmp, keyvalue_dup);
+
+    free_hash_table(a);
 
     return 0;
 }
